e5ef4be88259e883ddc60af9fba5d5432466079d
Bhavadharini Shankar <bhavakhil@gmail.com>
2026-01-13 23:49:54 +0530

    Time: 0 ms (100%), Space: 19.4 MB (5.39%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -2,8 +2,8 @@ class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         result=0
 
-        dic={"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8,"I":9,"J":10,"K":11,"L":12,"M":13,"N":14,"O":15,"P":16,"Q":17,"R":18,"S":19,"T":20,"U":21,"V":22,"W":23,"X":24,"Y":25,"Z":26}
+        
 
         for i in columnTitle:
-            result=result*26 + dic[i]
+            result=result*26 + (ord(i)-ord('A')+1)
         return result
